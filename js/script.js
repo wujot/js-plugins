4ffdc8e89bad216b3772a27fd3cf22fb46e4e06c
Wojciech Jeziorski <wojciech.wujot@gmail.com>
2019-01-15 00:04:43 +0100

    Add Flickity

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
+var elem = document.querySelector('.main-carousel');
+var flkty = new Flickity( elem, {
+  // options
+  cellAlign: 'left',
+  contain: true,
+  pageDots: false,
+  hash: true
+});
 
+// element argument can be a selector string
+//   for an individual element
+var flkty = new Flickity( '.main-carousel', {
+  // options
+
+});
+
+var progressBar = document.querySelector('.progress-bar')
+
+flkty.on( 'scroll', function( progress ) {
+  progress = Math.max( 0, Math.min( 1, progress ) );
+  progressBar.style.width = progress * 100 + '%';
+});
