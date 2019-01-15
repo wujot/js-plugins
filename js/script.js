4bdc6ae295e6c9c08a6cec3ab0d78f85e1ee421d
Wojciech Jeziorski <wojciech.wujot@gmail.com>
2019-01-15 22:10:48 +0100

    Add Google Map

--- js/script.js
+++ js/script.js
@@ -43,7 +43,28 @@ flkty.on( 'scroll', function( progress ) {
   progressBar.style.width = progress * 100 + '%';
 });
 
+// Definujemy funkcję initMap w zakresie globalnym (czyli jako właściwość obiektu window).
+  	window.initMap = function() {
+		
+		// Zapisujemy w zmiennej obiekt zawierający współrzędne geograficzne.
+		var uluru = {lat: 51.539011, lng: -0.142555};
+		
+		// W zmiennej map zapisujemy nową instancję obiektu Map. 
+		var map = new google.maps.Map(document.getElementById('map'), {
+			// Podajemy opcje mapy, np. zoom i punkt wycentrowania mapy.
+			zoom: 4,
+			center: uluru
+		});
 
-
+		for(var i = 0; i < slides.length; i++){ 
+			// Definiujemy marker jako nową instancję obiektu Marker.
+			var marker = new google.maps.Marker({
+				// I podajemy opcje tego markera, np. na której mapie ma być dodany oraz jakie są jego współrzędne. 
+				position: slides[i].coords,
+				map: map
+			}); 
+		}
+		
+	}	
 
 })(); 
