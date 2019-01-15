5b8b4cd76ba4a94acb814f9fae93f112293ee4a8
Wojciech Jeziorski <wojciech.wujot@gmail.com>
2019-01-15 21:06:50 +0100

    Add Mustache

--- js/script.js
+++ js/script.js
@@ -1,3 +1,25 @@
+'use strict';
+(function(){ 
+
+
+	var templateList = document.getElementById('template-slide-list').innerHTML;
+	var templateItem = document.getElementById('template-slide-item').innerHTML;
+	
+	Mustache.parse(templateItem); 
+	
+	var listItems = '';
+	
+	for(var i = 0; i < slides.length; i++){
+		listItems += Mustache.render(templateItem, slides[i]);
+	}
+	
+
+	var slideList = Mustache.render(templateList, {slides: listItems});
+	
+	
+	results.insertAdjacentHTML('beforeend', slideList);
+
+
 var elem = document.querySelector('.main-carousel');
 var flkty = new Flickity( elem, {
   // options
@@ -20,3 +42,8 @@ flkty.on( 'scroll', function( progress ) {
   progress = Math.max( 0, Math.min( 1, progress ) );
   progressBar.style.width = progress * 100 + '%';
 });
+
+
+
+
+})(); 
